2cd8f411bb8dbc7114d93d1a7468890b465b49d1
hf8441 <hf8441@bard.edu>
2016-08-16 15:07:01 -0400

    Update quote.js

--- workshop1/quote.js
+++ workshop1/quote.js
@@ -5,7 +5,7 @@
  */
 
 var things = ["Music", "Art", "Dance", "Theater", "Math", "Architecture"];
-var programmers = ["musician", "artist", "choreographer", "director", "mathematician", "architect"]
+var programmers = ["bagel", "treehouse", "dog", "cotton candy"]
 var castles = ["museums", "bridges", "cities"]
 var exertions = ["effort", "strain", "struggle", "toil", "endeavor", "hard work", "labor"];
 var airs = ["sky", "atmosphere", "heavens", "ether", "wind"];
