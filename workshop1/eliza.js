c4ecc222df5c11eebc50b4019ddf365c4db20711
hf8441 <hf8441@bard.edu>
2016-08-16 15:20:01 -0400

    Update eliza.js

--- workshop1/eliza.js
+++ workshop1/eliza.js
@@ -31,6 +31,10 @@ var rules = {
   "~*x hello ~*y": [
     "How do you do. Please state your problem."
   ],
+  "~*x dog ~*y": [
+  	"WOOF!",
+  	"Dogs are actually aliens."
+  ],
   "~*x computer ~*y": [
     "Do computers worry you?",
     "What do you think about machines have to do with your problem?",
